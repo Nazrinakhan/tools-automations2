20751b21306d0232ab48a6fe56cf090af50f29f9
Nazrina <nazrinakhan88@gmail.com>
2025-03-31 21:11:11 -0400

    Terrafrom

--- infra-create/main.tf
+++ infra-create/main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "tool"{
 
 }
 
-resource "aws_security_group" "tools-sg" {
+resource "aws_security_group" "tools-sg2" {
   name        = "${var.name}-sg"
   description = "${var.name}-sg"
 
